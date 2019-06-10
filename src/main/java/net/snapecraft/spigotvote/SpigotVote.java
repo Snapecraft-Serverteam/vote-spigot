27de3154ff66ddbb4cd113c2e6ead41af1a17e6c
Tallerik <erik.klasse@gmail.com>
2019-06-10 18:11:38 +0200

    Add Commands in config which runs on vote

--- src/main/java/net/snapecraft/spigotvote/SpigotVote.java
+++ src/main/java/net/snapecraft/spigotvote/SpigotVote.java
@@ -7,12 +7,26 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class SpigotVote extends JavaPlugin implements PluginMessageListener {
 
     @Override
     public void onEnable() {
         getServer().getMessenger().registerIncomingPluginChannel(this, "vote:votechannel", this);
+        initConfig();
 
+    }
+    private void initConfig() {
+        saveConfig();
+        reloadConfig();
+        List<String> cmds = new ArrayList<>();
+        cmds.add("/tellraw %PLAYER% [\"\",{\"text\":\"Danke für deinen Vote\",\"color\":\"red\"}]");
+        getConfig().addDefault("runonvote", cmds);
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        reloadConfig();
     }
 
     @Override
@@ -36,5 +50,11 @@ public final class SpigotVote extends JavaPlugin implements PluginMessageListene
     }
 
     private void sucessVote(Player target) {
+        saveConfig();
+        reloadConfig();
+        List<String> cmds = getConfig().getStringList("runonvote");
+        for (String cmd : cmds) {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd.replaceAll("%PLAYER%" , target.getName()));
+        }
     }
 }
